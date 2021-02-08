c4e20e72ad085314c8d17e11856779de42f3f00a
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-02-08 19:20:07 -0400

    Creando las migraciones, base de datos, configurando la base de datos, configurando el proyecto, agregando la carpeta Data y moviendo la base de datos a la carpeta anteriormente creada. Closes #4.

---
+++ Migrations/20210208231438_Inicial_Migracion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_apd1_20180906.Migrations
+{
+    public partial class Inicial_Migracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ciudad",
+                columns: table => new
+                {
+                    CiudadId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudad", x => x.CiudadId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ciudad");
+        }
+    }
+}
