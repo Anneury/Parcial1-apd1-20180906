3340308a4dee07902266809937d78678c38ae656
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-02-08 18:52:26 -0400

    Creando la clase Ciudad  con sus entidades y agregando la carpeta entidades. Closes #2.

---
+++ Entidades/Ciudad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_apd1_20180906.Entidades
+{
+    class Ciudad
+    {
+        [Key]
+        public int CiudadId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
